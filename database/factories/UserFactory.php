2a4d12ece871c8c175bf3ae71cb068e02ea59a96
Allan Rodrigues - Developer <consultoria01@shibata.corp.br>
2025-08-20 22:22:15 -0300

    Created Models Users And Projects

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,8 +15,6 @@ class UserFactory extends Factory
     protected static ?string $password;
 
     /**
-     * Define the model's default state.
-     *
      * @return array<string, mixed>
      */
     public function definition(): array
@@ -26,19 +22,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'avatar' => 'https://avatar.iran.liara.run/public/' . fake()->name() . '.png?size=100&bg=000000&fg=ffffff&text=',
+            'rate' => fake()->numberBetween(0, 100),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
